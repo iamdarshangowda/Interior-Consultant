840fb5bc1490ade37ad13f0d5b49b66fb8f27b7e
Darshan Gowda <iamdarshangowda@gmail.com>
2022-03-08 20:52:06 +0530

    navbar updated

--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
-const cross = "fa-duotone fa-xmark";
-const burger = "fa-burger fa-solid";
+const navToggle = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
 
-window.onload = ()=>{
-    const humMenu = document.querySelector('.hum-menu');
-    let clicked = true;
-    humMenu.addEventListener('click', ()=>{
-        if (clicked) 
-        {
-            document.querySelector('.line1').classList.add('line1-click');
-            document.querySelector('.line2').classList.add('line2-click');
-            document.querySelector('.line3').classList.add('line3-click');
-            document.querySelector('.navbar').classList.remove('hide');
-            document.querySelector('body').classList.add('navbar-overflow');
-            clicked = false;
-    }
-    else {
-        document.querySelector('.navbar').classList.add('hide');
-        document.querySelector('.line1').classList.remove('line1-click');
-        document.querySelector('.line2').classList.remove('line2-click');
-        document.querySelector('.line3').classList.remove('line3-click');
-        document.querySelector('body').classList.remove('navbar-overflow');
-        clicked = true;
-    }
-});
-}
+navToggle.addEventListener("click", function() {
+    links.classList.toggle("show-links");
+})
